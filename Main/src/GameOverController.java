e99ff3a8fe44d8767a9606b29f5dbba7e0b05cc2
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2017-11-12 00:40:59 +0530

    Displaying the winner

--- Main/src/GameOverController.java
+++ Main/src/GameOverController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -26,7 +27,7 @@ public class GameOverController {
 //    private URL location;
     public static Stage stage1;
     @FXML
-    private AnchorPane gamePane;
+    private BorderPane gamePane;
     @FXML
     void clickNewGame(ActionEvent event) throws IOException{
         GamePage.destroyGrid();
